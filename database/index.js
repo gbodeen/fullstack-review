f89c84ba8aebdcd63ddee2d69ee40d29e76c92a4
Gabriel Bodeen <gabebodeen@gmail.com>
2018-12-14 11:29:11 -0600

    Define repoSchema (untested).

--- database/index.js
+++ database/index.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+var db = mongoose.connection;
+db.on('error', () => console.log('Error establishing mongo db connectin.'));
+db.once('open', () => console.log('Mongo db connection established successfully.'));
+
+
+let repoSchema = new mongoose.Schema({
+  'id': Number,
+  'name': String,
+  'owner': String,
+  'ownerid': Number,
+  'url': String,
+  'description': String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
